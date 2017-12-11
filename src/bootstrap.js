87232706c74a9387738237df91f7344afd614a50
Amin Mohamed Ajani <amin.ajani17@gmail.com>
2017-12-11 23:15:43 +0530

    initial stage

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+(function(window){
+    window.log = function log() {
+        if (window.console && window.console.log) {
+            window.console.log.apply(window.console, arguments) // eslint-disable-line
+        }
+    }
+})(window);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+(function(window){
+    log("wassap");
+})(window)
